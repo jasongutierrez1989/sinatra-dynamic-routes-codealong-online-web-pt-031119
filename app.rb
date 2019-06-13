f9f9a92bd49b842477ebf2b5dc76e27fe77ac188
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-06-13 05:36:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,9 +19,9 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    @num1 = :num1
-    @num2 = :num2
-    @num1 * num2
+    @num1 = params[:num1]
+    @num2 = params[:num2]
+    @num1 * @num2
   end
 
 end
